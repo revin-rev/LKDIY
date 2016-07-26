4eceee0093d90fec257814da3389cde445a0ce0d
Anurag Makol <revinfotech1@gmail.com>
2016-07-26 17:26:48 +0530

    Refund pages updated

--- static/trade/scripts/step.js
+++ static/trade/scripts/step.js
@@ -1,7 +1,3 @@
 $('.trade-step-label').each(function(){
-    if($(this).is('.trade-step-label:last')){
-        $(this).css('right',- (($(this).width() / 2) - 16 ));
-    }else{
         $(this).css('left',- (($(this).width() / 2) - 16 ));
-    }
 });
