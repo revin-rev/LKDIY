707c12dcda30ff7e60fe08229fed3a533f79d624
revin-rev <revinfotech1@gmail.com>
2016-08-16 18:15:29 +0530

    Forget page added

---
+++ static/user/scripts/forget-password.js
@@ -0,0 +1,11 @@
+$('#radio2').click( function() {
+   $('#collapseTwo').show(); 
+   //$('#collapseTwo').css('height','128px');
+   $('#collapseOne').hide(); 
+
+});
+$('#radio1').click( function() {
+   $('#collapseTwo').hide(); 
+   $('#collapseOne').show(); 
+
+});
