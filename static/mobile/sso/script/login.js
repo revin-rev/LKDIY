3fda8d332f15982150d3799768da5d1a33e0c45f
revin-rev <revinfotech1@gmail.com>
2016-08-31 18:20:00 +0530

    Login page added

---
+++ static/mobile/sso/script/login.js
@@ -0,0 +1,65 @@
+jQuery(document).ready(function(){
+    jQuery('input[type=text],input[type=password]','.sso-form ').keyup(function(){
+        var _form = $(this).parents('form');
+        var _isValid = true;
+        var _input = $(_form).find('input[type=text],input[type=password]');
+
+        $(_input).each(function(){
+            if($(this).val() == '' || $(this).val() == null){
+                _isValid = false;
+            }
+        });
+        
+        if(_isValid){
+                    jQuery('.sso-button',_form).addClass('sso-enable');
+                    jQuery('.sso-button',_form).attr('disabled',true);
+        }else{
+                    jQuery('.sso-button',_form).removeClass('sso-enable');
+                    jQuery('.sso-button',_form).removeAttr('disabled');
+
+        }
+
+
+       
+        
+
+    });
+   jQuery('.sso-pass-eye').click(function(){
+       
+       var text = jQuery('.sso-password').val();
+       var img = jQuery(this).attr('src');
+   
+          if(text != '') {
+            
+          if(img == '../../static/mobile/sso/image/sso-eye.png'){
+               jQuery('.sso-password').attr('type','text');
+               jQuery('.sso-pass-eye').attr('src','../../static/mobile/sso/image/sso-blk-eye.png');
+           } else {
+               
+               jQuery('.sso-password').attr('type','password');
+               jQuery('.sso-pass-eye').attr('src','../../static/mobile/sso/image/sso-eye.png');
+           }
+       }
+   });
+
+
+  
+});
+
+ $(document).on('click','.open-3-modal', function () {
+    $.modal({
+      title:  '转分宝服务条款',
+     text: 'Vivamus feugiat diam velit. Maecenas aliquet egestas lacus, eget pretium massa mattis non. Donec volutpat euismod nisl in posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae',
+      buttons: [
+        {
+          text: 'B1',
+          onClick: function() {
+            $.alert('You clicked first button!')
+          }
+        }
+        
+        
+      ]
+    })
+    });
+
