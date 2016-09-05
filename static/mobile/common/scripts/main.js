3d0f64b667df4e5c7b03e7b98730bcf40c3a931c
revin-rev <revinfotech1@gmail.com>
2016-09-05 18:36:47 +0530

    Final commit 05-sep-16

--- static/mobile/common/scripts/main.js
+++ static/mobile/common/scripts/main.js
@@ -1,26 +1,28 @@
-var global = (function(){
-    var _o = {
-        backToTop: function(){
-            $('.content').scroll(function(){
-                if($('.content').scrollTop() > 150){
-                    $("[href='#top']").fadeIn();
-                }else{
-                    $("[href='#top']").fadeOut();
-                }
-            }).trigger('scroll');
+(function($){
+    var global = (function(){
+        var _o = {
+            backToTop: function(){
+                $('.content').scroll(function(){
+                    if($('.content').scrollTop() > 150){
+                        $("[href='#top']").fadeIn();
+                    }else{
+                        $("[href='#top']").fadeOut();
+                    }
+                }).trigger('scroll');
 
-            $("[href='#top']").click(function(){
-                $(".content").animate({ scrollTop: 0 }, 600);
-                return false;
-            });
-        },
-        init: function(){
-            _o.backToTop();
-        }
-    };
-    return _o;
-})();
+                $("[href='#top']").click(function(){
+                    $(".content").animate({ scrollTop: 0 }, 600);
+                    return false;
+                });
+            },
+            init: function(){
+                _o.backToTop();
+            }
+        };
+        return _o;
+    })();
 
-$(function(){
-    global.init();
-});
+    $(function(){
+        global.init();
+    });
+})(jQuery_noConflict)
