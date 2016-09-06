9485eade75dfefc0755b7c0c0343b29e888a9e51
revin-rev <revinfotech1@gmail.com>
2016-09-06 14:33:42 +0530

    Login page issues fixed

--- static/sso/scripts/login.js
+++ static/sso/scripts/login.js
@@ -12,17 +12,27 @@
         });
    });
 
-    $('.sui-validate input').blur(function(){
+    $('.sui-validate input').keyup(function(){
         var _form = $(this).parents('form');
+        var isValid  = true;
 
         $(_form).find('input').each(function(){
             var _this = $(this);
+
             setTimeout(function() {
                 if ($(_this).hasClass('input-error')) {
-                    $(_form).prop('style','padding-top:40px');
+                    isValid = false;
                 }
             },10);
         });
+
+        setTimeout(function(){
+            if(isValid){
+                $(_form).prop('style','padding-top:0');
+            }else{
+                $(_form).prop('style','padding-top:40px');
+            }
+        },11);
     });
 
 })(jQuery);  
