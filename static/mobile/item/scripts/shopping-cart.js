e8e0259f104fae2be21e808e7ccae649c39a9f92
revin-rev <revinfotech1@gmail.com>
2016-09-07 10:10:35 +0530

    Mobile pages added

---
+++ static/mobile/item/scripts/shopping-cart.js
@@ -0,0 +1,33 @@
+(function($){
+
+	 $(document).ready(function(){
+ 	
+ 	$('.increment').click(function(){
+ 		var value = $(this).prev().val();
+ 		if (!isNaN(value)){
+ 			value = parseInt(value) + 1;
+ 			$(this).prev().val(value);
+ 		}
+ 	});
+ 	$('.decrement').click(function(){
+ 		var value = $(this).next().val();
+ 		if (!isNaN(value) && value > 0){
+ 			value = parseInt(value)- 1;
+ 			$(this).next().val(value);
+ 		}
+ 	});
+ 	$("#checkAll").click(function () {
+ 		$('input:checkbox').not(this).prop('checked', this.checked);
+ 	});
+ 	$('.product-detail-text').click(function(){
+
+ 		$('.product-detail-text>p').fadeIn();
+
+ 		setTimeout(function(argument) {
+ 				$('.product-detail-text>p').fadeOut();
+ 		},2000);
+ 		
+ 	});
+ 	
+ });
+})(jQuery_noConflict)
