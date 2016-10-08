8676ca5a9214d45c4bd3a2ea3c7463536f8f8c85
revin-rev <mailto:revinfotech1@gmail.com>
2016-10-08 15:14:11 +0530

    mobile version js replaced

---
+++ static/mobile/common/scripts/main.js
@@ -0,0 +1,28 @@
+(function($){
+    var global = (function(){
+        var _o = {
+            backToTop: function(){
+                $('.content').scroll(function(){
+                    if($('.content').scrollTop() > 150){
+                        $("[href='#top']").fadeIn();
+                    }else{
+                        $("[href='#top']").fadeOut();
+                    }
+                }).trigger('scroll');
+
+                $("[href='#top']").click(function(){
+                    $(".content").animate({ scrollTop: 0 }, 600);
+                    return false;
+                });
+            },
+            init: function(){
+                _o.backToTop();
+            }
+        };
+        return _o;
+    })();
+
+    $(function(){
+        global.init();
+    });
+})(jQuery_noConflict)
